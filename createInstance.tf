8959fd2b6fec2b4431303b717fdf6eb033cb30cc
darkriver <b.n.gunes@gmail.com>
2025-01-14 15:47:39 +0100

    defining variables

--- createInstance.tf
+++ createInstance.tf
@@ -1,7 +1,10 @@
 
 resource "aws_instance" "MyFirstInstnace" {
+  count         = 1
+  ami           = "ami-05d38da78ce859165"
   instance_type = "t2.micro"
+
   tags = {
-    Name = "demoinstnce"
+    Name = "demoinstance"
   }
 }
